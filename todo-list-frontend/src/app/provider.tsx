b3dd7ced33a439f75da5f7e4ce2c2028cbdb0240
nKhanGh <khang.nguyenkhanghuu14@hcmut.edu.vn>
2026-07-05 15:49:27 +0700

    chore(frontend): setup tanstack provider

---
+++ todo-list-frontend/src/app/provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+import { Toaster } from "@/components/ui/sonner";
+
+export function Providers({ children }: Readonly<{ children: ReactNode }>) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <Toaster richColors position="top-right" />
+    </QueryClientProvider>
+  );
+}
