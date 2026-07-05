1c2525427f102b5e71f3bfea849d679d2b062056
nKhanGh <khang.nguyenkhanghuu14@hcmut.edu.vn>
2026-07-05 13:30:26 +0700

    Add MapStruct todo mapper

---
+++ todo-list-backend/src/main/java/com/khangdev/todolistbackend/todo/mapper/TodoMapper.java
@@ -0,0 +1,19 @@
+package com.khangdev.todolistbackend.todo.mapper;
+
+import com.khangdev.todolistbackend.todo.dto.response.TodoResponse;
+import com.khangdev.todolistbackend.todo.dto.response.TodoStatusHistoryResponse;
+import com.khangdev.todolistbackend.todo.entity.Todo;
+import com.khangdev.todolistbackend.todo.entity.TodoStatusHistory;
+import java.util.List;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface TodoMapper {
+
+    TodoResponse toResponse(Todo todo);
+
+    TodoStatusHistoryResponse toResponse(TodoStatusHistory history);
+
+    List<TodoStatusHistoryResponse> toHistoryResponses(List<TodoStatusHistory> histories);
+}
